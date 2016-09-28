7c993dce3c426954a96750593fc55943cca896a4
github username <github email address>
2016-09-28 16:52:24 -0400

    Please push :C

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,22 +1,32 @@
 require 'rest-client'
+require 'JSON'
+require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
+
+def character_hash
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  # iterate ove the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film. 
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list 
-  #  of movies by title. play around with puts out other info about a given film.
+  JSON.parse(all_characters)
 end
 
-def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def find_character(character)
+   character_hash.values.last.find do |character_hash_element|
+     character_hash_element["name"].downcase == character.downcase
+   end
+end
+
+def get_character_movies_from_api(character)
+     find_character(character)["films"].map { |url| JSON.parse(RestClient.get(url))}
+
+end
+
+
+
+def parse_character_movies(character)
+
+  # get_character_movies_from_api(character).each_with_index {|hash, index| puts "#{index + 1}. #{hash["title"]}"}
+  character.each_with_index {|hash, index| puts "#{index + 1}. #{hash["title"]}"}
+
+
 end
 
 def show_character_movies(character)
